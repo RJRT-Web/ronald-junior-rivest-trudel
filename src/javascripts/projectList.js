34e4bf8787b61fb720e4bdbe3d0dce872f6b88ce
Ronald Junior Rivest-Trudel <ronaldjunior.rivesttrudel@hotmail.com>
2025-07-28 15:08:52 -0400

    Modified project description

--- src/javascripts/projectList.js
+++ src/javascripts/projectList.js
@@ -5,7 +5,7 @@ const projectList = [
   {
     name: "Movie Library",
     description:
-      "Generating a movie library with React using the TMDB API, useState, useEffect and useContent.",
+      "Créer une bibliothèque de films avec React en utilisant l'API TMDB, les hooks useState, useEffect et useContent.",
     link: "https://movie-library-orcin.vercel.app/",
     image: LibraryMovie,
     icons: ["html", "css", "javascript", "react"],
@@ -13,7 +13,8 @@ const projectList = [
   },
   {
     name: "RPG Game",
-    description: "Description of Project A",
+    description:
+      "Mini jeux avec javascript qui permet de simuler un combat style RPG",
     link: "https://rjrt-web.github.io/rpg-combat/",
     image: RpgGameImg,
     icons: ["html", "css", "javascript"],
